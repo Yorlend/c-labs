ca5e35720418a4c90b8ed284e1574503df0e2c68
kkyu20u548 <139klim@gmail.com>
2021-02-22 12:21:55 +0300

    lab_01_03_02 initial commit

---
+++ lab_01_03_02/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    double r1, r2, r3, r;
+
+    printf("Input R1, R2, R3: ");
+    scanf("%lf%lf%lf", &r1, &r2, &r3);
+
+    r = r1*r2*r3/(r1*r2 + r1*r3 + r2*r3);
+    printf("Circuit resistance: %.6lf\n", r);
+
+    return 0;
+}
