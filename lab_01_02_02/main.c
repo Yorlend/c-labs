a1ce64f42a211755ea377e40dcac7f9e9cb1465e
kkyu20u548 <139klim@gmail.com>
2021-02-22 12:07:09 +0300

    lab_01_02_02 initial commit

---
+++ lab_01_02_02/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double x_a, x_b, x_c, y_a, y_b, y_c, ab, ac, bc, P;
+    printf("Enter coordinates of the triangle:\n");
+    scanf("%lf%lf%lf%lf%lf%lf", &x_a, &y_a, &x_b, &y_b, &x_c, &y_c);
+    
+    ab = sqrtf((x_a - x_b)*(x_a - x_b) + (y_a - y_b)*(y_a - y_b));
+    bc = sqrtf((x_b - x_c)*(x_b - x_c) + (y_b - y_c)*(y_b - y_c));
+    ac = sqrtf((x_a - x_c)*(x_a - x_c) + (y_a - y_c)*(y_a - y_c));
+
+    if (ab >= (bc + ac) || bc >= (ac + ab) ||
+         ac >= (bc + ab))
+        printf("No triangle\n");
+    else
+    {
+    P = ab + bc + ac;
+    printf("Perimeter of a triangle is: %.6lf\n", P);
+    }
+
+    return 0;
+}
