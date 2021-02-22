627fd98a05f341e2e6dbe5d304230177783bfedb
kkyu20u548 <139klim@gmail.com>
2021-02-22 13:06:35 +0300

    Fixed Job #162580 testing  errors

--- lab_01_03_02/main.c
+++ lab_01_03_02/main.c
@@ -4,10 +4,9 @@ int main()
 {
     double r1, r2, r3, r;
 
-    printf("Input R1, R2, R3: ");
     scanf("%lf%lf%lf", &r1, &r2, &r3);
 
-    r = r1*r2*r3/(r1*r2 + r1*r3 + r2*r3);
+    r = r1 * r2 * r3 / (r1 * r2 + r1 * r3 + r2 * r3);
     printf("Circuit resistance:\n R=%.6lf Ohm\n", r);
 
     return 0;
