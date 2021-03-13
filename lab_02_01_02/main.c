a2b3529caf725127d9df29009610841038d5f093
kkyu20u548 <139klim@gmail.com>
2021-03-13 10:48:28 +0300

    lab_02_01_02 initial version

---
+++ lab_02_01_02/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+#define N 10
+
+int input_array(int *arr, int size);
+double average_neg(int *arr, int size);
+
+int main(void)
+{
+    int arr[N], size;
+    double result = 0.;
+
+    scanf("%d", &size);
+    if (size > N)
+        return 1;
+
+    if (input_array(arr, size) == 1)
+        return 1;
+    
+    result = average_neg(arr, size);
+
+    printf("%lf", result);
+
+    return 0;
+}
+
+int input_array(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+        if (scanf("%d", &arr[i]) != 1)
+            return 1;
+
+    return 0;
+}
+
+double average_neg(int *arr, int size)
+{
+    double res = 0.;
+    int n = 0, sum = 0;
+
+    for (int i = 0; i < size; i++)
+        if (arr[i] < 0)
+        {
+            n++;
+            sum += arr[i];
+        }
+    if (n)
+        res = (double)sum / n;
+
+    return res;
+}
