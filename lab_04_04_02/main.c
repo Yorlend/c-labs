0ff7b1ff0c49469951bee8e1e1a6189d86566772
kkyu20u548 <139klim@gmail.com>
2021-05-09 20:26:05 +0300

    lab_04_04_02 init

---
+++ lab_04_04_02/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include "my_string.h"
+#include "date_check.h"
+
+int main(void)
+{
+    int exit_code = SUCCESS;
+    char str[MAX_STR_LEN] = "";
+    char split_s[MAX_STR_LEN] = "";    
+
+    char words[MAX_WORDS_COUNT][MAX_WORD_LEN];
+    int size = 0;
+
+    exit_code = input_string(str);
+
+    strncpy(split_s, str, MAX_STR_LEN);
+
+    if (!exit_code)
+        exit_code = split_str(words, &size, split_s);
+
+    if (!exit_code)
+    {
+        char *pstr = str;
+        date_t date = { 0, 0, 0 };
+        if (!parse_space_mb(&pstr) ||\
+            !parse_date(&date, &pstr) ||\
+            !parse_space_mb(&pstr))
+            printf("NO\n");
+        else
+            printf("YES\n");
+    }
+
+    return exit_code;
+}
---
+++ lab_04_04_02/my_string.h
@@ -0,0 +1,30 @@
+#ifndef MY_STRING
+#define MY_STRING
+#define MAX_STR_LEN 257
+#define MAX_WORD_LEN 17
+#define MAX_WORDS_COUNT 128
+#define SUCCESS 0
+#define ERROR 1
+#define DELIMS " ,;:-.!?"
+
+#include <stdbool.h>
+
+int input_string(char *str);
+int split_str(char (*str_array)[MAX_WORD_LEN], int *size, char *str);
+
+/**
+ * str = "13bcd"
+ * parse_uint(..., &str) == true
+ * str == "bcd"
+ * 
+ * str = "abcd"
+ * parse_uint(..., &str) == false
+ * str == "abcd"
+ */
+bool parse_uint(int *value, char **str);
+bool parse_char(char **str, char chr);
+bool parse_word(char *word, char **str);
+bool parse_space(char **str);
+bool parse_space_mb(char **str);
+
+#endif
