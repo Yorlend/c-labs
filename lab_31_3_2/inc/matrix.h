d27a5b8bf99ac0cdd2b0a03837fed1401d97f922
Klim Kornienko <139klim@gmail.com>
2021-11-27 09:57:41 +0300

    add comments

--- lab_31_3_2/inc/matrix.h
+++ lab_31_3_2/inc/matrix.h
@@ -14,19 +14,62 @@ typedef struct matrix
 } matrix_t;
 
 
+/**
+ * @brief Создает пустую матрицу (null)
+ * 
+ * @return matrix_t 
+ */
 matrix_t mat_null(void);
+
+/**
+ * @brief Освобождает память из-под матрицы
+ * 
+ * @param mat (матрица, память которой очищается)
+ */
 void mat_free(matrix_t* mat);
 
+/**
+ * @brief Проверка - корректна ли матрица (не null)
+ * 
+ * @param mat проверяемая матрица
+ * @return true 
+ * @return false 
+ */
 bool mat_is_null(const matrix_t* mat);
 
-matrix_t mat_zero(int rows, int cols);
-
+/**
+ * @brief Читает матрицу из файла по файловому указателю
+ * 
+ * @param file файловый указатель
+ * @param mat выходная матрица
+ * @return int (код ошибки)
+ */
 int mat_readf(FILE* file, matrix_t* mat);
+
+/**
+ * @brief Читает матрицу по имени файла
+ * 
+ * @param mat выходная матрица
+ * @param filename имя входного файла
+ * @return int (код ошибки)
+ */
 int mat_read(matrix_t* mat, const char* filename);
 
+/**
+ * @brief Выводит матрицу в файл / на экран (stdout)
+ * 
+ * @param file выходной файл
+ * @param mat матрица
+ * @return int (код ошибки)
+ */
 int mat_writef(FILE* file, matrix_t* mat);
 
-// Решает задачу сортировки необходимых элементов в массиве
+/**
+ * @brief Решает задачу сортировки необходимых элементов в массиве
+ * 
+ * @param mat исходная матрица
+ * @return int (код ошибки)
+ */
 int mat_operate(matrix_t* mat);
 
 #endif /* __MATRIX_H__ */
