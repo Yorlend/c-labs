6ba561712d5a2399941122427dbe3bf6694a36e1
kkyu20u548 <139klim@gmail.com>
2022-02-22 22:32:11 +0300

    init labs

---
+++ test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo INPUT NUMBER OF POSITIVE TESTS:
+read POS
+echo INPUT NUMBER OF NEGATIVE TESTS:
+read NEG
+
+cd debug
+
+rm -rf gcov
+
+for i in $(seq 1 $POS)
+do
+    IN=$(printf "../func_tests/pos_%02d_in.txt" $i)
+    OUT=$(printf "../func_tests/pos_%02d_out.txt" $i)
+    ./$1 < $IN > temp.txt
+    diff temp.txt $OUT >> debug.txt
+    if [ $? == 1 ]
+    then
+        printf "TEST pos_%02d failed\n" $i
+    else
+        printf "TEST pos_%02d passed\n" $i
+    fi
+done
+
+echo
+
+for i in $(seq 1 $NEG)
+do
+    IN=$(printf "../func_tests/neg_%02d_in.txt" $i)
+    OUT=$(printf "../func_tests/neg_%02d_out.txt" $i)
+    ./$1 < $IN > temp.txt
+    ERROR=$?
+    if [[ `cat $OUT` == $ERROR ]]
+    then
+        printf "TEST neg_%02d passed\n" $i
+    else
+        printf "TEST neg_%02d failed\n" $i
+    fi
+done
+
+for src in ../*.c
+do
+    cp $src .
+    gcov -o . -f -p $src
+done
+
+rm -rf gcov *.gcda *.gcno *.c
+mkdir gcov && mv *.gcov gcov
+
+cd ..
